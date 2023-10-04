50a930fe37d13df5c139bba301b5d330c4511e09
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2023-10-04 16:10:26 -0400

    commit tree 9054c50e736eb1dc609b1c0102c95e54ca0b1204

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(arr, str){
+  let result = arr.filter((element) => element.toLowerCase() === str.toLowerCase());
+  return result;
+}
+
+function fuzzyMatch(arr, str){
+  let result = arr.filter((element) => element[0].toLowerCase() === str[0].toLowerCase())
+  return result;
+}
+
+function matchName(arr, str){
+  let result = arr.filter((element) => element.name.toLowerCase() === str.toLowerCase());
+  return result;
+}
+
+// Code your soution here
